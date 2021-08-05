d39b1ee4649383587247196480c28d6b4d1b9b07
Vento <vento.michael0705@hotmail.com>
2021-08-05 21:28:45 +0200

    add list of catches

--- src/components/captures/CaptureList.jsx
+++ src/components/captures/CaptureList.jsx
@@ -1,24 +1,24 @@
-import React, { useState, useEffect } from 'react';
+import React, {useState, useContext, useEffect} from 'react';
 import {Link, Redirect } from "react-router-dom";
 import firebase from '../../config/config.jsx';
+import {AuthContext} from '../../context/Auth.jsx';
 
 const CaptureList = () => {
     const [leState, setleState] = useState(null);
     const [isLogged, setisLogged] = useState([]);
-    const user = firebase.auth().currentUser;
-    const uid = user.uid;
+    const {currentUser} = useContext(AuthContext);
     useEffect(() =>
     {
         const fetchData = async () => {
             const db = firebase.firestore();
-            const data = await db.collection('captures').where("userUid", "==", uid).get();
+            const data = await db.collection('catches').where('userUid', '==', currentUser ? currentUser.uid : '').get();
             setleState (data.docs.map (doc => ({...doc.data(), id: doc.id})));
         };
+
         fetchData();
     }, []);
-
     firebase.auth().onAuthStateChanged(function(user) {
-        if (user) {
+        if (currentUser) {
             setisLogged(true);
         } else {
             setisLogged(false);
@@ -34,25 +34,20 @@ const CaptureList = () => {
         <React.Fragment>
             <section>
                 <div>
-                    <h2>Toutes mes captures</h2>
-                    <ul className="list">
+                    <h2 aria-level="2">Toutes mes captures</h2>
+                    <ul className="list__new__catches">
                         {leState.map(bird => (
-                            <li key={bird.id} className="list__item">
-                                <Link to={{pathname: '/captures/' + bird.id}}><span className="hidden">Plus d'informations sur {bird.name}</span></Link>
-                                <Link to={{pathname: '/edit/' + bird.id}} className="icon"><span className="sro">Modifier</span>
-                                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
-                                         fill="none" stroke="#606a73" strokeWidth="1.5" strokeLinecap="round"
-                                         strokeLinejoin="round" className="feather feather-edit-3">
-                                        <path d="M12 20h9"></path>
-                                        <path d="M16.5 3.5a2.121 2.121 0 0 1 3 3L7 19l-4 1 1-4L16.5 3.5z"></path>
-                                    </svg>
+                            <li key={bird.id} className="list__catch_edit">
+                                <Link className="link__back" to={{pathname: '/captures/' + bird.id}}>Plus d'informations sur {bird.name}</Link>
+                                <Link className="link__back" to={{pathname: '/edit/' + bird.id}}>Modifier {bird.name}
                                 </Link>
                             </li>
                         ))}
                         {leState.length === 0 &&
-                        <li className="empty">Vous n'avez pas encore capturé d'oiseau&nbsp;!</li>
+                        <li>Oops, encore aucune capture d'oiseau&nbsp;!</li>
                         }
                     </ul>
+                    <Link to='/capture/new' className="link__back link__add__new_catch">Ajouter une nouvelle capture</Link>
                 </div>
             </section>
 
