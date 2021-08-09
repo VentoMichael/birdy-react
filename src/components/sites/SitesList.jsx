55243c06cafc5720bd489af792dd5e8268ce5c93
Vento <vento.michael0705@hotmail.com>
2021-08-09 17:18:23 +0200

    listing sites of all users

---
+++ src/components/sites/SitesList.jsx
@@ -0,0 +1,70 @@
+import React, {useState, useEffect} from 'react';
+import {Link, Redirect} from "react-router-dom";
+import firebase from '../../config/config';
+import GMap from '../common/GoogleMap.jsx';
+import {Map, GoogleApiWrapper, Marker} from 'google-maps-react';
+
+const SiteList = (props) => {
+    const [leState, setleState] = useState(null);
+    const [long, setLong] = useState(null);
+    const [lat, setLat] = useState(null);
+    const [isLogged, setisLogged] = useState([]);
+
+    useEffect(() => {
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition((position) => {
+                setLat(position.coords.latitude);
+                setLong(position.coords.longitude);
+            });
+        } else {
+            alert("Geolocation is not supported by this browser.");
+        }
+
+        const fetchData = async () => {
+            const db = firebase.firestore();
+            const data = await db.collection('sites').get();
+            setleState(data.docs.map(doc => ({...doc.data(), id: doc.id})));
+        };
+        fetchData();
+    }, []);
+
+    firebase.auth().onAuthStateChanged(function (user) {
+        if (user) {
+            setisLogged(true);
+        } else {
+            setisLogged(false);
+        }
+    });
+    if (isLogged === false) {
+        return <Redirect to='/'/>
+    }
+    if (leState === null) {
+        return <Redirect to='/sites'/>
+    }
+    return (
+        <React.Fragment>
+            <section>
+                <div>
+                    <div className="list__catch_edit">
+                        <h2>Sites de baguage</h2>
+                        <Link className="link__back" to={'/sites/new'}>Nouveau site</Link>
+                    </div>
+                    <div className="container__login container__listes">
+                        {leState.map(map => (
+                            <div key={map.id}>
+                                <p>Endroit du site : <span>{map.name}</span></p>
+                                <p>Superficie : <span>{map.superficie} <abbr title="kilomètre">km</abbr></span></p>
+                                <GMap initialPos={{lat: map.geopoint.latitude, lng: map.geopoint.longitude}} zoom={10}
+                                      getCoordinates={true}/>
+                            </div>
+                        ))}
+                    </div>
+                </div>
+            </section>
+
+        </React.Fragment>
+    )
+};
+export default GoogleApiWrapper({
+    apiKey: 'AIzaSyBLbc_IMKUfMTdokXqOWLMCXErOUJTwhX4'
+})(SiteList);
